5f5e99fe2e84d8164789afb4458618c0ea60d094
Marius C. K <marius_gulbrandsen@hotmail.no>
2020-07-26 00:51:36 +0200

    Added input field zoom button to adjust text size.

---
+++ Assets/Scripts/InputFieldZoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldZoom : MonoBehaviour
+{
+    public Button plusButton;
+    public Button minusButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button plusBtn = plusButton.GetComponent<Button>();
+        Button minusBtn = minusButton.GetComponent<Button>();
+        
+        // Add click function to button
+        plusBtn.onClick.AddListener(OnButtonClick);
+        minusBtn.onClick.AddListener(OnButtonClick);
+    }
+
+    void OnButtonClick()
+    {
+        Debug.Log("Button clicked!");
+    }
+}
