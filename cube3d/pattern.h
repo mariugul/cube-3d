caafc596b6b4da58162522e769fc93bd3e6ed9f5
Marius C. K <marius_gulbrandsen@hotmail.no>
2020-08-01 16:42:24 +0200

    Create pattern.h

---
+++ cube3d/pattern.h
@@ -0,0 +1,22 @@
+#ifndef __PATTERN_H__
+#define __PATTERN_H__
+
+// Includes
+//---------------------------------
+#include <stdint.h>        // Use uint_t
+#include <avr/pgmspace.h>  // Store patterns in program memory
+
+// Pattern that LED cube will display
+//--------------------------------- 
+const PROGMEM uint16_t pattern_table[] = {
+     0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 10,
+     0x0000, 0x0000, 0x0000, 0x0000, 10,
+     0xFFFF, 0xFFFF, 0xFFFF, 0xFFFF, 100000,
+     0x0000, 0x0000, 0x0000, 0x0000, 10,
+     0x0000, 0x0000, 0x0000, 0x0000, 10,
+     0x0000, 0x0000, 0x0000, 0x0000, 10,
+     0x0000, 0x0000, 0x0000, 0x0000, 10,
+     0x0000, 0x0000, 0x0000, 0x0000, 10,
+     0x0000, 0x0000, 0x0000, 0x0000, 10,
+};
+#endif
