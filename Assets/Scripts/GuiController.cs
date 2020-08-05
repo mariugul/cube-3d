314bad94471cab3de13321adaacb4117ed2ff829
Marius C. K <marius_gulbrandsen@hotmail.no>
2020-08-05 19:05:09 +0200

    Added a more complete (empty) GUI.
    This is the approximately the setup I want for the first release.

--- Assets/Scripts/GuiController.cs
+++ Assets/Scripts/GuiController.cs
@@ -7,6 +7,9 @@ public class GuiController : MonoBehaviour
 	void Start()
 	{
 		var form = new WinFormToolStrip(panel);
+		var form2 = new WinFormSettings();
+
 		form.Show();
+		//form2.Show();
 	}
 }
--- Assets/Scripts/WinFormToolStrip.cs
+++ Assets/Scripts/WinFormToolStrip.cs
@@ -2,6 +2,10 @@
 using System.Drawing;
 using System.Windows.Forms;
 using TMPro;
+using UnityEngine.WSA;
+using System;
+using System.IO;
+using System.Diagnostics;
 
 public class WinFormToolStrip : Form
 {
@@ -28,16 +32,49 @@ public class WinFormToolStrip : Form
         var itemExport   = new ToolStripMenuItem("Export");
         var itemSettings = new ToolStripMenuItem("Settings");
         var itemAbout    = new ToolStripMenuItem("About");
-
+        var itemDiscord  = new ToolStripMenuItem("Discord");
+        var itemYoutube  = new ToolStripMenuItem("Youtube");
+        var itemHotkeys  = new ToolStripMenuItem("Hotkeys");
 
         // Tool strip subitems
-        var itemFileOpen = new ToolStripMenuItem("Open");
+        var itemExportArduinoProject = new ToolStripMenuItem("Arduino Project");
+        var itemExportPatternFile    = new ToolStripMenuItem("Pattern File");
+        var itemExportSelectFolder   = new ToolStripMenuItem("Select Default");
+
+        var itemFileSave = new ToolStripMenuItem("Save Project");
         var itemFileExit = new ToolStripMenuItem("Exit");
 
-        // On click functionality
-        itemFileOpen.Click += (sender, args) =>
+        // On click Hotkeys
+        itemHotkeys.Click += (sender, args) =>
+        {
+            MessageBox.Show(
+                "Generate pattern              Enter\n\n"            +
+                "Enable all LEDs               Ctrl + A\n"         +
+                "Disable all LEDs              Ctrl + Shift + A\n\n" +
+                "Select pattern time           Ctrl + T\n\n"         +
+                "Redo generated line       Ctrl + Z\n"         +
+                "Delete pattern line           Delete\n");
+        };
+
+        // On click Youtube
+        itemYoutube.Click += (sender, args) =>
+        {
+            Process.Start("https://www.youtube.com");
+        };
+
+
+        // On click Discord
+        itemDiscord.Click += (sender, args) =>
+        {
+            Process.Start("https://www.discord.com");
+        };
+
+
+        // On click SELECT FOLDER
+        itemExportSelectFolder.Click += (sender, args) =>
         {
             var ofd = new OpenFileDialog();
+            
             var result = ofd.ShowDialog(); // Saving the result is pointless, it's not async stuff here.
                                            // What you can do is wait for form closing:
             ofd.FormClosed += (o, eventArgs) =>
@@ -47,8 +84,38 @@ public class WinFormToolStrip : Form
                     UnityEngine.Debug.Log(ofd.FileName);
                 }
             };
+            
         };
 
+        // On click FILE SAVE
+        itemFileSave.Click += (sender, args) =>
+        {
+            var sfd = new SaveFileDialog();
+
+            sfd.Filter = "C Header File|*.h";
+
+            var result = sfd.ShowDialog();
+
+            sfd.FormClosed += (o, eventArgs) =>
+            {
+                if (sfd.DialogResult == DialogResult.OK)
+                {
+                    if (!File.Exists(sfd.FileName))
+                    {
+                        File.Create(sfd.FileName);
+                        MessageBox.Show("The file has been created.");
+                    }
+                    else
+                    {
+                        File.Create(sfd.FileName);
+                        MessageBox.Show("The file has been overwritten.");
+                    }
+                }
+            };
+
+        };
+
+        // On click ABOUT
         itemAbout.Click += (sender, args) =>
         {
             string html_link = "https://github.com/mariugul/cube-3d";
@@ -60,14 +127,21 @@ public class WinFormToolStrip : Form
            
         };
 
+        // On click SETTINGS
         itemSettings.Click += (sender, args) => {
             panel.TogglePanel();
             //MessageBox.Show("Settings!");
         };
 
-        // Add items to dropdown menu
-        itemFile.DropDownItems.Add(itemFileOpen);
+        // Add items to FILE dropdown menu
+        itemFile.DropDownItems.Add(itemFileSave);
         itemFile.DropDownItems.Add(itemFileExit);
+
+        // Add items to EXPORT dropdown menu 
+        itemExport.DropDownItems.Add(itemExportArduinoProject);
+        itemExport.DropDownItems.Add(itemExportPatternFile);
+        //itemExport.DropDownItems.Add(itemExportSelectFolder); // Wait with this for next release
+
 
         // Set up menu strip and anchor at the top
         var menu = new MenuStrip();
@@ -81,7 +155,9 @@ public class WinFormToolStrip : Form
         menu.Items.Add(itemExport);
         menu.Items.Add(itemSettings);
         menu.Items.Add(itemAbout);
-
+        menu.Items.Add(itemDiscord);
+        menu.Items.Add(itemYoutube);
+        menu.Items.Add(itemHotkeys);
         // Add menu strip
         Controls.Add(menu);
     }
