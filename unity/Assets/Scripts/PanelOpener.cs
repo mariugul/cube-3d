a46a9b7513202f067f1a72cc6de046f5e6c9e6a7
Marius C. K <marius_gulbrandsen@hotmail.no>
2020-08-31 02:41:14 +0200

    Add code editor

--- unity/Assets/Scripts/PanelOpener.cs
+++ unity/Assets/Scripts/PanelOpener.cs
@@ -5,16 +5,33 @@ using UnityEngine.UI;
 public class PanelOpener : MonoBehaviour
 {
     public GameObject panel;
+    public GameObject editor;
 
     public void TogglePanel()
     {
+        bool editorIsActive   = editor.activeSelf;
+
         if (panel != null)
         {
-            // Read panel state
-            bool isActive = panel.activeSelf;
-
             // Set state to the oposite
-            panel.SetActive(!isActive);
+            panel.SetActive(!panel.activeSelf);
+        }
+
+        if (editor != null)
+        { 
+            // Settings have just been opened
+            if (panel.activeSelf)
+            {
+                // Switch off code editor 
+                editor.SetActive(false);
+            }
+
+            // Settings was just closed
+            else
+            {
+                // Switch on code editor
+                editor.SetActive(true);
+            }
         }
     }
 }
